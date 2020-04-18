e4325b0bef87bbebd93339bc0d7af6e212b65691
tetsu-upstr <bodomu@gmail.com>
2020-04-19 08:23:53 +0900

    try catch

--- dom/form/main.js
+++ dom/form/main.js
@@ -10,9 +10,35 @@
 // console.log(formTest2);
 
 // form.（form id）.（input name）で要素を取得できる
-const formTestYourname = document.forms.form_test1.your_name;
-console.log(formTestYourname);
+const formTestYourName = document.forms.form_test1.your_name;
+console.log(formTestYourName);
 
 // form.（form id）.（input name）.valueで値を取得できる
-const formTestYournameValue = document.forms.form_test1.your_name.value;
-console.log(formTestYournameValue);
+const formTestYourNameValue = document.forms.form_test1.your_name.value;
+console.log(formTestYourNameValue);
+
+// 入力した文字を別の場所に表示させる
+formTestYourName.addEventListener('input', () => {
+  let yourNameValue = document.getElementById('your_name_value');
+  yourNameValue.textContent = formTestYourName.value;
+});
+
+// const input = createElement('input');
+// input.value = 'ああ';
+// input.name = 'your_name';
+// input.type = 'text';
+
+const preview = document.forms.form_test2.preview;
+
+document.forms.form_test2.image_file.addEventListener('change', (e) => {
+  console.log(e.target.files[0]);
+  const uploadFile = e.target.files[0];
+
+  // FileReader
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    preview.src = reader.result; // Base64（アドレス）
+    console.log(preview.src);
+  });
+  reader.readAsDataURL(uploadFile);
+});
